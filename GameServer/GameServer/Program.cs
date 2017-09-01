87c205bb90080feb9370688eea73097bece5026a
Perberos <perberos@gmail.com>
2017-08-31 23:25:35 -0300

    Agrego: Solución GameServer

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prevenir que corra más de una instancia del juego para evitar corrupcion al cargar o guardar
+            if (AlreadyRunning()) return;
+            // se carga el juego
+            Game game = new Game();
+
+            game.load();
+
+            // se inicia el puerto de escucha para ServerListener o ServerNodeListener o como sea que se llame jaja ServerPapasFritasListener :v
+            ConnectionsListener listen = new ConnectionsListener();
+            listen.start();
+
+            // game loop - parte logica y loop infinito
+            game.loop();
+        }
+    }
+}
